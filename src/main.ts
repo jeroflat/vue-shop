14ae334936cd72dd687d88056372ff1d358d468c
jeroflat <jeronimo.velasquez@glovoapp.com>
2022-06-24 16:17:05 +0200

    refactor(main.ts): add store

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from '@/components/core/App.vue';
 
-// import store from '@store';
+import store from '@/store';
 import { router } from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
