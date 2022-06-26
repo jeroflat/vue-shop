fe55177d2605efd059d930c918ad0ff609343326
jeroflat <jeronimo.velasquez@glovoapp.com>
2022-06-26 23:29:17 +0200

    refactor(app): convert to setup

--- src/components/core/App.vue
+++ src/components/core/App.vue
@@ -3,15 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from 'vue';
 import BaseLayout from './BaseLayout.vue';
-
-defineComponent({
-  name: 'App',
-  components: {
-    BaseLayout,
-  },
-});
 </script>
 
 <style lang="scss">
