ff11db479da46f0d98f1742b0bd938c4d411251f
jeroflat <jeronimo.velasquez@glovoapp.com>
2022-06-27 18:25:26 +0200

    refactor(home): use refactored <ProductCard />

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -2,21 +2,21 @@
   <section>
     <h1 class="display-3 text-center mt-3">Shop</h1>
 
-    <!-- <div class="row">
+    <div class="row">
       <ProductCard
         v-for="product in products"
         :key="product.id"
-        :width="'45%'"
         :product="product"
-        hasverlay
+        has-hover
+        width="45%"
       />
-    </div> -->
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { createNamespacedHelpers } from 'vuex-composition-helpers';
-// import ProductCard from '@/components/ProductCard.vue';
+import ProductCard from '@/components/ProductCard.vue';
 
 const { useState, useActions } = createNamespacedHelpers('products');
 const { fetchProducts } = useActions(['fetchProducts']);
